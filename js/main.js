13b601ff5d00a5feb9092899e83e14aee65a3232
Jean-Luc Montreuil <jlmontr@gmail.com>
2016-12-27 10:58:53 -0500

    WIP redesign - index

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(window).scroll( function(){
         var bottom_of_object = $(this).position().top + $(this).outerHeight()
         var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-        /* If the object is completely visible in the window, fade it it */
+        /* If the object is completely visible in the window, fade it in */
         if( bottom_of_window > bottom_of_object ){
 
             $(this).animate({'opacity':'1'},500);
